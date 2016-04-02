d358551f9a877cbca79ac938ea0a0f92191cb44e
thneang <thneang@gmail.com>
2016-04-02 04:58:53 +0200

    example json

--- code.3d/src/lray.cpp
+++ code.3d/src/lray.cpp
@@ -2,37 +2,49 @@
 #include <json/json.h>
 
 
+void output(const Json::Value & value)
+{
+    // querying the json object is very simple
+    std::cout << value["hello"];
+    std::cout << value["number"];
+    std::cout << value["array"][0] << value["array"][1];
+    std::cout << value["object"]["hello"];
+}
 
 
 int main(int argc, char** argv)
 { 
-Json::Value root;   // 'root' will contain the root value after parsing.
-std::cin >> root;
-// You can also read into a particular sub-value.
-std::cin >> root["subtree"];
-// Get the value of the member of root named 'encoding',
-// and return 'UTF-8' if there is no such member.
-std::string encoding = root.get("encoding", "UTF-8" ).asString();
-// Get the value of the member of root named 'plug-ins'; return a 'null' value if
-// there is no such member.
-//const Json::Value plugins = root["plug-ins"];
-// Iterate over the sequence elements.
-//for ( int index = 0; index < plugins.size(); ++index )
- //  loadPlugIn( plugins[index].asString() );
-   
-// Try other datatypes. Some are auto-convertible to others.
-//foo::setIndentLength( root["indent"].get("length", 3).asInt() );
-//foo::setIndentUseSpace( root["indent"].get("use_space", true).asBool() );
-// Since Json::Value has an implicit constructor for all value types, it is not
-// necessary to explicitly construct the Json::Value object.
-//root["encoding"] = foo::getCurrentEncoding();
-//root["indent"]["length"] = foo::getCurrentIndentLength();
-//root["indent"]["use_space"] = foo::getCurrentIndentUseSpace();
-// If you like the defaults, you can insert directly into a stream.
-std::cout << root;
-// Of course, you can write to `std::ostringstream` if you prefer.
-// If desired, remember to add a linefeed and flush.
-std::cout << std::endl;
+// ---- create from scratch ----
+
+Json::Value fromScratch;
+Json::Value array;
+array.append("hello");
+array.append("world");
+fromScratch["hello"] = "world";
+fromScratch["number"] = 2;
+fromScratch["array"] = array;
+fromScratch["object"]["hello"] = "world";
+
+output(fromScratch);
+
+// write in a nice readible way
+Json::StyledWriter styledWriter;
+std::cout << styledWriter.write(fromScratch);
+
+// ---- parse from string ----
+
+// write in a compact way
+Json::FastWriter fastWriter;
+std::string jsonMessage = fastWriter.write(fromScratch);
+
+Json::Value parsedFromString;
+Json::Reader reader;
+bool parsingSuccessful = reader.parse(jsonMessage, parsedFromString);
+if (parsingSuccessful)
+{
+    std::cout << styledWriter.write(parsedFromString) << std::endl;
+}
+
 
 
   return 0;
